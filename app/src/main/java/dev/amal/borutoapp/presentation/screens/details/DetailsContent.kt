05eb7d1862e0f54e1a801d406d2f2ef826dd8c46
Just_Amalll <amalkrasavchik@gmail.com>
2021-12-18 23:22:00 +0600

    Details Screen - Animate Sheet Radius

--- app/src/main/java/dev/amal/borutoapp/presentation/screens/details/DetailsContent.kt
+++ app/src/main/java/dev/amal/borutoapp/presentation/screens/details/DetailsContent.kt
@@ -1,12 +1,15 @@
 package dev.amal.borutoapp.presentation.screens.details
 
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -15,6 +18,7 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import coil.annotation.ExperimentalCoilApi
 import coil.compose.rememberImagePainter
@@ -40,7 +44,13 @@ fun DetailsContent(
 
     val currentSheetFraction = scaffoldState.currentSheetFraction
 
+    val radiusAnim by animateDpAsState(
+        targetValue = if (currentSheetFraction == 1f) EXTRA_LARGE_PADDING
+        else EXPANDED_RADIUS_LEVEL
+    )
+
     BottomSheetScaffold(
+        sheetShape = RoundedCornerShape(topStart = radiusAnim, topEnd = radiusAnim),
         scaffoldState = scaffoldState,
         sheetPeekHeight = MIN_SHEET_HEIGHT,
         sheetContent = {
